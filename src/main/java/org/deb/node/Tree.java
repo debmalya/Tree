ca060d197b8f1f7c79b6c1f7b51c2d3ba153c0dc
Debmalya Jash <debmalya.jash@gmail.com>
2018-04-10 22:24:57 +0800

    Update Tree.java

--- src/main/java/org/deb/node/Tree.java
+++ src/main/java/org/deb/node/Tree.java
@@ -13,5 +13,24 @@ public class Tree<T> {
 
     Tree<T> left;
     Tree<T> right;
+    
+    /**
+	 * 
+	 * @param t
+	 * @return
+	 */
+	public boolean isTreeSymmetric(Tree<T> t) {
+		if (t != null) {
+			return mirrorEquals(t.left, t.right);
+		}
+		return true;
+	}
+
+	public boolean mirrorEquals(Tree<T> left, Tree<T> right) {
+		if (left == null || right == null)
+			return left == null && right == null;
+		return left.value == right.value && mirrorEquals(left.left, right.right)
+				&& mirrorEquals(left.right, right.left);
+	}
 
 }
